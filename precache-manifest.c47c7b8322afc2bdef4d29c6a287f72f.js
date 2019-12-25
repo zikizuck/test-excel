756e96fdadbb4dfee46f281ec8cf83a4bbb78545
Ziki Zuck <ziki.zuck@gmail.com>
2019-12-25 07:49:43 +0200

    Updates

---
+++ precache-manifest.c47c7b8322afc2bdef4d29c6a287f72f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "224131befcbe8969e576ed669ee5aa49",
+    "url": "/test-excel/index.html"
+  },
+  {
+    "revision": "edf5b1aedd2091f19297",
+    "url": "/test-excel/static/css/2.a1f70397.chunk.css"
+  },
+  {
+    "revision": "7407011b8b28a2598453",
+    "url": "/test-excel/static/css/main.9538b3e2.chunk.css"
+  },
+  {
+    "revision": "edf5b1aedd2091f19297",
+    "url": "/test-excel/static/js/2.651093ea.chunk.js"
+  },
+  {
+    "revision": "a34ff0125cab7c32bc50ba794786f65d",
+    "url": "/test-excel/static/js/2.651093ea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7407011b8b28a2598453",
+    "url": "/test-excel/static/js/main.b1ccb5a4.chunk.js"
+  },
+  {
+    "revision": "56e60f349e0655a90081",
+    "url": "/test-excel/static/js/runtime-main.2daa89fe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-excel/static/media/logo.5d5d9eef.svg"
+  }
+]);
