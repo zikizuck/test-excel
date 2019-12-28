c0d75c0a4cf174366fb1cfba7d61d0d504a3d518
Ziki Zuck <ziki.zuck@gmail.com>
2019-12-28 11:44:46 +0200

    איפוס

--- src/CallApi/CallApi.js
+++ src/CallApi/CallApi.js
@@ -44,7 +44,7 @@ render() {
                 <form className="form-inline my-2 my-lg-0">
                     <input className="form-control mr-sm-2" type="search" placeholder="לפי מקט" onChange={this.clickHandler.bind(this)}/>
                     <input className="form-control mr-sm-2" type="search" placeholder="לפי תאור מוצר" onChange={this.search2Handler.bind(this)}/>
-                    {/*<button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>*/}
+                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">איפוס</button>
                 </form>
                 <div className="card-deck">
                     {this.state.product
