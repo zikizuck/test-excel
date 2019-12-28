d667242e9d3a726bf6ce3b8523b3fe8175310232
Ziki Zuck <ziki.zuck@gmail.com>
2019-12-28 09:29:49 +0200

    with hebrew

--- src/CallApi/CallApi.js
+++ src/CallApi/CallApi.js
@@ -42,9 +42,9 @@ render() {
 
             <div>
                 <form className="form-inline my-2 my-lg-0">
-                    <input className="form-control mr-sm-2" type="search" placeholder="Search" onChange={this.clickHandler.bind(this)}/>
-                    <input className="form-control mr-sm-2" type="search" placeholder="By description" onChange={this.search2Handler.bind(this)}/>
-                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    <input className="form-control mr-sm-2" type="search" placeholder="לפי מקט" onChange={this.clickHandler.bind(this)}/>
+                    <input className="form-control mr-sm-2" type="search" placeholder="לפי תאור מוצר" onChange={this.search2Handler.bind(this)}/>
+                    {/*<button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>*/}
                 </form>
                 <div className="card-deck">
                     {this.state.product
