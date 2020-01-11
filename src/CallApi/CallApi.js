414eeb7cdadd4562a9127bc995165942c15beab7
Ziki Zuck <ziki.zuck@gmail.com>
2020-01-11 08:04:48 +0200

    add onKeyPress

--- src/CallApi/CallApi.js
+++ src/CallApi/CallApi.js
@@ -44,7 +44,7 @@ render() {
 
                 <div className="container">
                     <form  className="form-inline my-2 my-lg-0">
-                        <input className="form-control mr-sm-2" type="search" placeholder="לפי מקט" onChange={this.clickHandler.bind(this)}/>
+                        <input className="form-control mr-sm-2" type="search" placeholder="לפי מקט" onChange={this.clickHandler.bind(this)} onKeyPress={(e)=>{e.key === 'Enter'&& e.preventDefault()}}/>
                         <input className="form-control mr-sm-2" type="search" placeholder="לפי תאור מוצר" onChange={this.search2Handler.bind(this)}/>
                         <button className="btn btn-outline-success my-2 my-sm-0" type="submit">איפוס</button>
                     </form>
