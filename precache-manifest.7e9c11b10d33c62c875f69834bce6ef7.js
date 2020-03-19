f4689a090a7e26f04269e4a51d3a08bddf599242
Ziki Zuck <ziki.zuck@gmail.com>
2020-03-19 20:19:13 +0200

    Updates

---
+++ precache-manifest.7e9c11b10d33c62c875f69834bce6ef7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d8bbc41018c700ac76adae5e717ea4c",
+    "url": "/test-excel/index.html"
+  },
+  {
+    "revision": "65fdaf1c67f41460be49",
+    "url": "/test-excel/static/css/2.a1f70397.chunk.css"
+  },
+  {
+    "revision": "35d5086efe505b5b4c76",
+    "url": "/test-excel/static/css/main.99663355.chunk.css"
+  },
+  {
+    "revision": "65fdaf1c67f41460be49",
+    "url": "/test-excel/static/js/2.454cda25.chunk.js"
+  },
+  {
+    "revision": "551147300d50ee76730b3f071752d663",
+    "url": "/test-excel/static/js/2.454cda25.chunk.js.LICENSE"
+  },
+  {
+    "revision": "35d5086efe505b5b4c76",
+    "url": "/test-excel/static/js/main.3fdf7487.chunk.js"
+  },
+  {
+    "revision": "56e60f349e0655a90081",
+    "url": "/test-excel/static/js/runtime-main.2daa89fe.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/test-excel/precache-manifest.7b172e8316008685c6cd4542149e4bab.js"
+  "/test-excel/precache-manifest.7e9c11b10d33c62c875f69834bce6ef7.js"
 );
 
 self.addEventListener('message', (event) => {
